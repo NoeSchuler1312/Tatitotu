b0ea6478b0fc36d4069a0cffd6531738117b0b8d
NoeSchuler1312 <122873508+NoeSchuler1312@users.noreply.github.com>
2024-03-27 09:34:07 +0100

    Début des menus topnav
    menu année + 5P

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,19 @@
+//menu année
 var dropdownYears = document.querySelector(".dropdown-years");
 var menuYears = document.querySelector(".menu-years");
 
-var toggleMenu = function toggleMenu() {
+var toggleMenuYears = function toggleMenuYears() {
   menuYears.classList.toggle("is-active");
 };
 
-dropdownYears.addEventListener("click", toggleMenu);
+dropdownYears.addEventListener("click", toggleMenuYears);
+
+//menu année
+var dropdown5p = document.querySelector(".dropdown-5p");
+var menu5p = document.querySelector(".menu-5p");
+
+var toggleMenu5p = function toggleMenu5p() {
+  menu5p.classList.toggle("is-active");
+};
+
+dropdown5p.addEventListener("click", toggleMenu5p);
